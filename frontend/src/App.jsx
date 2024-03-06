cf05c0e81dcb302c39e27481498358eb4ce99d8e
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-03-06 16:35:09 +0300

    loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Lottie from "react-lottie";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import Work from "./components/Work";
@@ -7,23 +8,55 @@ import Talk from "./components/Talk";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import { Context } from "./Helpers/Helper";
-import { useState } from "react";
+import loadings from "./images/loading.json";
+import { useEffect, useState } from "react";
 
 function App() {
   const [changeMode, setChangeMode] = useState(false);
+  const [loadingPage, setLoadingPage] = useState(true);
+  useEffect(() => {
+    const pageTimer = setTimeout(() => {
+      setLoadingPage(false);
+    }, [2000]);
+    return () => clearTimeout(pageTimer);
+  }, [loadingPage]);
+
   return (
     <div className={`${changeMode ? "bg-white text-black" : "bg-slate-800 text-white"}`}>
-      <Context.Provider value={{ changeMode, setChangeMode }}>
-        <Navigation />
+      {loadingPage ? (
+        <h1>
+          {" "}
+          <span className="flex items-center gap-0 justify-center p-[25%]">
+            Wait! Loading...
+            <Lottie
+              options={{
+                loop: true,
+                autoplay: true,
+                animationData: loadings,
+                rendererSettings: {
+                  preserveAspectRatio: "xMidYMid slice",
+                },
+              }}
+              width={100}
+              height={100}
+            />
+          </span>
+        </h1>
+      ) : (
+        <div>
+          <Context.Provider value={{ changeMode, setChangeMode }}>
+            <Navigation />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/talk" element={<Talk />} />
-        </Routes>
-        <Footer />
-      </Context.Provider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/work" element={<Work />} />
+              <Route path="/talk" element={<Talk />} />
+            </Routes>
+            <Footer />
+          </Context.Provider>
+        </div>
+      )}
     </div>
   );
 }
